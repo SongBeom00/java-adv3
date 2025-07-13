716a45550f2322d19fa7d8d843d92f3b2ef75dcc
SongBeom00 <kjb001023@naver.com>
2025-07-13 14:41:03 +0900

    :sparkles: Interface Function

---
+++ src/lambda/lambda3/GenericMain6.java
@@ -0,0 +1,24 @@
+package lambda.lambda3;
+
+public class GenericMain6 {
+
+    public static void main(String[] args) {
+        GenericFunction<String,String> toUpperCase = str -> str.toUpperCase();
+        GenericFunction<String,Integer> stringLength = str -> str.length();
+        GenericFunction<Integer,Integer> square = x -> x*x;
+        GenericFunction<Integer,Boolean> isEvan = num -> num % 2 == 0;
+
+        System.out.println(toUpperCase.apply("hello"));
+        System.out.println(stringLength.apply("hello"));
+        System.out.println(square.apply(3));
+        System.out.println(isEvan.apply(3));
+
+    }
+
+    @FunctionalInterface
+    interface GenericFunction<T, R>{
+        R apply(T s);
+    }
+
+
+}
